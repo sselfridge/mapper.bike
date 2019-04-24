0cf83830ab3e011c6f23454a15529c2b727d1dfb
Sam Selfridge <Sam.selfridge@gmail.com>
2019-04-23 18:07:23 -0700

    Inital commit - things are wokring now

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+module.exports = {
+
+    mode: process.env.NODE_ENV,
+    entry: "./client/index.js",
+
+    output: {
+        path: path.resolve(__dirname, 'build/'),
+        filename: "bundle.js",
+    },
+
+    module: {
+        rules: [
+          {
+            test: /\.jsx?$/,
+            exclude: /(node_modules)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env','@babel/preset-react']
+              }
+            }
+          },
+          {
+            test: /\.css$/,
+            use: [
+              {
+                loader: "style-loader" // creates style nodes from JS strings
+              },
+              {
+                loader: "css-loader" // translates CSS into CommonJS
+              },
+              {
+                loader: "sass-loader" // compiles Sass to CSS
+              }
+            ]
+          },
+        ]
+      },
+    devServer: {
+      publicPath: '/build/',
+      proxy: {
+        '/api': 'http://localhost:3000',
+        secure: false
+      },
+      
+    }
+};
