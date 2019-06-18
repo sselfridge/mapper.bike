eaa1b47cf926bcf8847c11606ef41df9cc88785b
Sam Selfridge <Sam.selfridge@gmail.com>
2019-06-17 18:13:24 -0700

    Adding console to index serve

--- server/server.js
+++ server/server.js
@@ -92,6 +92,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use('/build', express.static(path.join(__dirname, '../build')));
   // serve index.html on the route '/'
   app.get('/', (req, res) => {
+    console.log("Sending out the index");
     res.sendFile(path.join(__dirname, '../index.html'));
   });
 }
