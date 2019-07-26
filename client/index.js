52a8d0e4c05b181ab619ce730c2a772f9148b090
Sam Selfridge <Sam.selfridge@gmail.com>
2019-07-26 10:50:17 -0700

    comment for future

--- client/index.js
+++ client/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
 
+// VScode says this is unused but it is mistaken
 import styles from './styles.css';
 
 render(
