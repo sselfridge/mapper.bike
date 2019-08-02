8d6259be762f29f37761901c38844087c31c2df7
Sam Selfridge <Sam.selfridge@gmail.com>
2019-08-01 18:22:32 -0700

    Webpack doesn't like staging as an env name

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,60 @@
-const path = require('path');
-const webpack = require('webpack');
-const GitRevisionPlugin = require('git-revision-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const GitRevisionPlugin = require("git-revision-webpack-plugin");
 const gitRevisionPlugin = new GitRevisionPlugin();
 
+const env =
+  process.env.NODE_ENV === "staging" ? "production" : process.env.NODE_ENV;
+
 module.exports = {
+  mode: env,
+  entry: "./client/index.js",
 
-    mode: process.env.NODE_ENV,
-    entry: "./client/index.js",
+  output: {
+    path: path.resolve(__dirname, "build/"),
+    filename: "bundle.js"
+  },
 
-    output: {
-        path: path.resolve(__dirname, 'build/'),
-        filename: "bundle.js",
-    },
-
-    module: {
-        rules: [
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
           {
-            test: /\.jsx?$/,
-            exclude: /(node_modules)/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env','@babel/preset-react']
-              }
-            }
+            loader: "style-loader" // creates style nodes from JS strings
           },
           {
-            test: /\.css$/,
-            use: [
-              {
-                loader: "style-loader" // creates style nodes from JS strings
-              },
-              {
-                loader: "css-loader" // translates CSS into CommonJS
-              },
-              {
-                loader: "sass-loader" // compiles Sass to CSS
-              }
-            ]
+            loader: "css-loader" // translates CSS into CommonJS
           },
+          {
+            loader: "sass-loader" // compiles Sass to CSS
+          }
         ]
-      },
-    devServer: {
-      publicPath: '/build/',
-      proxy: {
-        '/api': 'http://localhost:3000',  //added [::1] because voodoo https://github.com/saikat/react-apollo-starter-kit/issues/20
-        secure: false
-      },
-      
-    },
-    plugins: [
-      new webpack.DefinePlugin({
-        'VERSION': JSON.stringify(gitRevisionPlugin.version()),
-        'COMMITHASH': JSON.stringify(gitRevisionPlugin.commithash()),
-        'BRANCH': JSON.stringify(gitRevisionPlugin.branch()),
-      })
+      }
     ]
+  },
+  devServer: {
+    publicPath: "/build/",
+    proxy: {
+      "/api": "http://localhost:3000", //added [::1] because voodoo https://github.com/saikat/react-apollo-starter-kit/issues/20
+      secure: false
+    }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      VERSION: JSON.stringify(gitRevisionPlugin.version()),
+      COMMITHASH: JSON.stringify(gitRevisionPlugin.commithash()),
+      BRANCH: JSON.stringify(gitRevisionPlugin.branch())
+    })
+  ]
 };
