3b0b304360f64134695203eae3b709d017dba8ba
Sam Selfridge <Sam.selfridge@gmail.com>
2019-08-01 18:13:03 -0700

    adding things for a staging ENV

--- config/keys_DUMMY.js
+++ config/keys_DUMMY.js
@@ -7,23 +7,44 @@ prodKeys = {
   client_secret: "",
   callback_uri: "",
   redirect_url: "",
-  //jwt token
-  secretSuperKey: "",
+  twilioTo: ""
+};
 
-  mapsApi: ""
+stagingKeys = {
+  client_id: "",
+  client_secret: "",
+  callback_uri: "",
+  redirect_url: "",
+  twilioTo: ""
 };
 
 devKeys = {
   client_id: "",
-  client_secret: " ",
+  client_secret: "",
   callback_uri: "",
   redirect_url: "",
-  //jwt token
-  secretSuperKey: "",
-
-  mapsApi: ""
+  twilioTo: ""
 };
 
-const keys = env === "production" ? prodKeys : devKeys;
+let keys;
+
+switch (env) {
+  case "production":
+    keys = prodKeys;
+    break;
+  case "staging":
+    keys = stagingKeys;
+    break;
+  default:
+    keys = devKeys;
+    break;
+}
+
+keys.secretSuperKey = "";
+keys.mapsApi = "";
+keys.twilioKey = "";
+keys.twilioAccount = "";
+keys.twilioNum = "";
+keys.squirrelKey = "";
 
 module.exports = keys;
