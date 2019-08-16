b868cc6de713ec3eebd4136638d8123150f01b33
Sam Selfridge <Sam.selfridge@gmail.com>
2019-08-16 09:06:35 -0700

    update deploy script

--- pull.sh
+++ pull.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-git pull
-npm install
-npm run build
+# create command array
+arr=( "git pull")
+arr+=("npm install")
+arr+=("npm run build")
+arr+=("pm2 restart map")
+
+for i in "${arr[@]}"
+do
+    eval $i
+    if [ $? != 0 ]
+    then
+        echo 'Error - exiting script'
+        exit 1
+    fi
+done
