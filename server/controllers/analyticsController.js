a66b23e3757d7935151a9eb70a5dfa0e930d466f
Sam Selfridge <Sam.selfridge@gmail.com>
2019-08-25 23:56:41 -0700

    Adding time to analytics logger

--- server/controllers/analyticsController.js
+++ server/controllers/analyticsController.js
@@ -4,7 +4,7 @@ const analyticsController = {};
 analyticsController.getUserData = getUserData;
 
 function getUserData(req, res, next) {
-  fs.appendFileSync("logs/analytics.txt", `${req.headers["user-agent"]}\n`);
+  fs.appendFileSync("logs/analytics.txt", `${req.headers["user-agent"]} - ${new Date().toLocaleString()}\n`);
   next();
 }
 
