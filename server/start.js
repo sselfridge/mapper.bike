017a6d29b3c4f4cf82a03147bfaf8f3540b4d7b7
Sam Selfridge <Sam.selfridge@gmail.com>
2020-09-13 12:47:43 -0700

    listen to proper port in prod

--- server/start.js
+++ server/start.js
@@ -1,6 +1,9 @@
-const app = require('./server')
+const app = require("./server");
 
 //split off the listen() function so the server can be loaded for testing
+const env = process.env.NODE_ENV;
 
-app.listen(3000)
-console.log(`Listening on Port 3000`);
+const ip = env === "production" ? 80 : 3000;
+
+app.listen(ip);
+console.log(`Listening on Port ${ip}`);
