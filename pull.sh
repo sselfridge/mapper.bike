f1961612fcab0389fed448ab7b45cafa27f5951a
Sam Selfridge <Sam.selfridge@gmail.com>
2021-10-02 15:42:36 -0700

    update pull

--- pull.sh
+++ pull.sh
@@ -2,24 +2,51 @@
 
 
 # create command array
-arr=( "git pull")
-arr+=("npm install")
-arr+=("REACT_APP_GIT_HASH=$(git rev-parse --short HEAD) npm run build")
-arr+=("pm2 restart ecosystem.config.js")
-arr+=("git log -1")
+# arr=( "git pull")
+# arr+=("npm install")
+# arr+=("REACT_APP_GIT_HASH=$(git rev-parse --short HEAD) npm run build")
+# arr+=("pm2 restart ecosystem.config.js")
+# arr+=("git log -1")
+
+git pull
+if [ $? != 0 ]
+then
+    echo 'Error - exiting script'
+    exit 1
+fi
+npm install
+if [ $? != 0 ]
+then
+    echo 'Error - exiting script'
+    exit 1
+fi
+REACT_APP_GIT_HASH=$(git rev-parse --short HEAD) npm run build
+if [ $? != 0 ]
+then
+    echo 'Error - exiting script'
+    exit 1
+fi
+pm2 restart ecosystem.config.js
+if [ $? != 0 ]
+then
+    echo 'Error - exiting script'
+    exit 1
+fi
+git log -1
+
 
 # $(git rev-parse --short HEAD) GIT_HASH=$(git rev-parse --short HEAD) pm2 restart ecosystem.config.js
 
 # pm2 command for map:
 # NODE_ENV=production pm2 start --name map node server/server.js
 
-for i in "${arr[@]}"
-do
-    eval $i
-    if [ $? != 0 ]
-    then
-        echo 'Error - exiting script'
-        exit 1
-    fi
-done
+# for i in "${arr[@]}"
+# do
+#     eval $i
+#     if [ $? != 0 ]
+#     then
+#         echo 'Error - exiting script'
+#         exit 1
+#     fi
+# done
 
