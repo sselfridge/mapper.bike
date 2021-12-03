8dfe14842ab7ff9cb0ac7b5f2e549e8152b3cc9e
Sam Selfridge <sam.selfridge@gmail.com>
2021-12-03 12:02:23 -0800

    Revert "debugging on prod"
    This reverts commit c9049a45bf258ac945082aa9ae6e7a55da3fa348.

--- src/utils/helperFunctions.js
+++ src/utils/helperFunctions.js
@@ -1,10 +1,7 @@
 //shared functions used both in server and UI
 
 const appendLeaderboard = (activityId, leaderboard) => {
-  if (activityId == 4115863174) console.info("activityId: ", activityId);
-  if (activityId == 4115863174) console.info("leaderboard: ", leaderboard);
   const current = leaderboard.find((e) => e.activityId === `${activityId}`);
-  if (activityId == 4115863174) console.info("current: ", current);
 
   if (current) {
     return parseInt(current.place, 10);
